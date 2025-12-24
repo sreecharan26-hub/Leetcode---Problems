6656a12e6f6bc10975a87db9bf5bbac1ab42140c
Sree Charan <sreecharanpaila26@gmail.com>
2025-12-24 21:28:15 +0530

    Time: 0 ms (100%), Space: 26.3 MB (7.19%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        map<int, int> mpp;
-        for (auto i : nums) {
-            mpp[i]++;
-        }
-        for (auto it : mpp) {
-            if (it.second == 1) {
-                return (it.first);
+    int singleNonDuplicate(vector<int>& arr) {
+        int n = arr.size();
+        int l = 0;
+        int r = n-1;
+        for(int i=0;i<n;i++){
+            if(n==1){
+                return arr[0];
+            }
+            if(i==0){
+                if(arr[i]!=arr[i+1])
+                return arr[0];
+            }
+            else if(i==n-1){
+                if(arr[i]!=arr[i-1])
+                return arr[i];
+            }
+            else{
+                if(arr[i]!=arr[i+1] and arr[i]!= arr[i-1])
+                return arr[i];
             }
         }
-
-        return -1;
+        return 0;
     }
 };
