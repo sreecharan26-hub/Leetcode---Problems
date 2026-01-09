976c32b83be96ace53676657660621324146ab33
Sree Charan <sreecharanpaila26@gmail.com>
2026-01-09 15:16:29 +0530

    Time: 0 ms (100%), Space: 24.4 MB (69.4%) - LeetHub

---
+++ 3688-bitwise-or-of-even-numbers-in-an-array/3688-bitwise-or-of-even-numbers-in-an-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int evenNumberBitwiseORs(vector<int>& nums) {
+        int n = nums.size();
+        int a = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]%2==0){
+                a|=nums[i];
+            }
+        }
+        return a;
+    }
+};
