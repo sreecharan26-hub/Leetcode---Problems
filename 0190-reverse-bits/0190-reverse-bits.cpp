e61745f5dfaae097d7ec7b02dc5d6c853b4ae0a6
Sree Charan <sreecharanpaila26@gmail.com>
2026-02-06 20:38:18 +0530

    Time: 4 ms (29.46%), Space: 8.1 MB (73.12%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        int ans = 0;
+        int ans =0;
         for(int i=0;i<32;i++){
-            ans = (ans<<1)|(n&1);
-            n=n>>1;
-        } 
+            ans = ans*2+(n%2);
+            n=n/2;
+        }
         return ans;
     }
 };
