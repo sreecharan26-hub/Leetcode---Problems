118654695edb8ee8ede3a1db0a69264b1b8f7746
Sree Charan <sreecharanpaila26@gmail.com>
2026-03-03 17:46:09 +0530

    Time: 6 ms (68.1%), Space: 19.7 MB (27.7%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& nums) {
+        int n = nums.size();
+         stack<int>st;
+         int maxarea = 0;
+         for(int i=0;i<n;i++){
+            while(!st.empty() and nums[st.top()]>nums[i]){
+                int element = nums[st.top()];
+                st.pop();
+                int nse = i;
+                int pse; 
+                if(st.empty()){
+                    pse = -1;
+                }
+                else{
+                    pse=st.top();
+                }
+                maxarea = max(maxarea,(nse-pse-1)*element);
+            }
+            st.push(i);
+         }
+         while(!st.empty()){
+            int nse = n;
+            int element =nums[st.top()];
+            st.pop();
+            int pse;
+            if(st.empty()){
+                pse = -1;
+            }
+            else{
+                pse=st.top();
+            }
+            maxarea=max(maxarea,(nse-pse-1)*element);
+         }
+         return maxarea;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if(matrix.empty()){
+            return 0;
+        }
+        int a = matrix.size();
+        int b = matrix[0].size();
+        vector<int>nums(b,0);
+        int maxi=0;
+        for(int i=0;i<a;i++){
+            for(int j=0;j<b;j++){
+                if(matrix[i][j]=='1'){
+                    nums[j]++;
+                }
+                else{
+                    nums[j]=0;
+                }
+            }
+            maxi = max(maxi,largestRectangleArea(nums));
+
+        }
+        return maxi;
+        
+    }
+};
