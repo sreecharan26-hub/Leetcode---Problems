45a6c6e13fc9b183d6b73a9f3958a12a778760d0
Sree Charan <sreecharanpaila26@gmail.com>
2026-03-11 19:37:49 +0530

    Time: 0 ms (100%), Space: 7.8 MB (59.24%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -4,18 +4,10 @@ public:
         if(n==0){
             return 1;
         }
-        vector<int>v;
-        while(n>0){
-            v.push_back(n%2);
-            n/=2;
+        int a = 1;
+        while(a<=n){
+            a=a<<1;
         }
-        for(int i=0;i<v.size();i++){
-            v[i]=1-v[i];
-        }
-        int ans = 0;
-        for(int i=0;i<v.size();i++){
-            ans+=v[i]*(1<<i);
-        }
-        return ans;
+        return (a-1)-n;
     }
 };
