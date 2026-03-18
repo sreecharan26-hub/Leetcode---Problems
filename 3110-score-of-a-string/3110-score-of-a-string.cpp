1a71c1fda0ad4fab9acc3a9be8c897a246af1adb
Sree Charan <sreecharanpaila26@gmail.com>
2026-03-18 12:36:44 +0530

    Time: 0 ms (100%), Space: 8.9 MB (65.82%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -4,7 +4,7 @@ public:
         int n = s.size();
         int sum =0;
         for(int i=0;i<n-1;i++){
-            int a =abs(s[i]-s[i+1]);
+            int a =abs((s[i]-'a')-(s[i+1]-'a'));
             sum+=a;
         }
         return sum;
