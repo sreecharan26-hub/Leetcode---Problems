90accfd76544e5048f2e7c68f940c0a8becaebc8
Sree Charan <sreecharanpaila26@gmail.com>
2026-03-26 18:54:47 +0530

    Time: 6 ms (75.96%), Space: 44.8 MB (47.52%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -7,15 +7,13 @@ public:
         int m = s.size();
         int i = 0;
         int j =0;
-        int cnt = 0;
         while(i<n and j<m){
             if(g[i]<=s[j]){
-                cnt++;
                 i++;
             }
             j++;
         }
-        return cnt;
+        return i;
 
     }
 };
