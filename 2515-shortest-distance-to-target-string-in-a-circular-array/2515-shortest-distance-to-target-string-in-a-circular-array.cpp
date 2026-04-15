171ede1eeafee25ed0d73fae66f86d9888719d59
Sree Charan <sreecharanpaila26@gmail.com>
2026-04-15 20:36:20 +0530

    Time: 0 ms (100%), Space: 18.4 MB (37.22%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -5,8 +5,10 @@ public:
         int ans = INT_MAX;
         for(int i=0;i<n;i++){
             if(words[i]==target){
-                ans = min(ans,abs(i-startIndex));
-                ans = min(ans,n-abs(i-startIndex));
+               int front = (i-startIndex+n)%n;
+               int back = (startIndex-i+n)%n;
+               int dist=min(front,back);
+               ans = min(ans,dist);
             }
         }
         if(ans == INT_MAX){
