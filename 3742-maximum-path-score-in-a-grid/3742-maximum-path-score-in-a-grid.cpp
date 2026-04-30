c5f07a852f8480eb4aaff3be9dc1f1cc5a4a614d
Sree Charan <sreecharanpaila26@gmail.com>
2026-04-30 06:46:01 +0530

    Time: 710 ms (86.59%), Space: 499.4 MB (46.52%) - LeetHub

---
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int maxPathScore(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        // dp[i][j][c] = max score
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(k + 1, -1)));
+
+        // start point
+        dp[0][0][0] = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int c = 0; c <= k; c++) {
+                    if (dp[i][j][c] == -1) continue;
+
+                    // move right
+                    if (j + 1 < n) {
+                        int val = grid[i][j + 1];
+                        int cost = (val == 0 ? 0 : 1);
+                        int score = val;
+
+                        if (c + cost <= k) {
+                            dp[i][j + 1][c + cost] = max(
+                                dp[i][j + 1][c + cost],
+                                dp[i][j][c] + score
+                            );
+                        }
+                    }
+
+                    // move down
+                    if (i + 1 < m) {
+                        int val = grid[i + 1][j];
+                        int cost = (val == 0 ? 0 : 1);
+                        int score = val;
+
+                        if (c + cost <= k) {
+                            dp[i + 1][j][c + cost] = max(
+                                dp[i + 1][j][c + cost],
+                                dp[i][j][c] + score
+                            );
+                        }
+                    }
+                }
+            }
+        }
+
+        int ans = -1;
+        for (int c = 0; c <= k; c++) {
+            ans = max(ans, dp[m - 1][n - 1][c]);
+        }
+
+        return ans;
+    }
+};
