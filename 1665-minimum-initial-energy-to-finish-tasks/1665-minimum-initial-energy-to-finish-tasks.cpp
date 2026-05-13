b04efe0a44488ab285fd0dd0cced5bcc6b0a1ad1
Sree Charan <sreecharanpaila26@gmail.com>
2026-05-13 10:55:53 +0530

    Time: 57 ms (42.51%), Space: 97.2 MB (99.03%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
+    bool isPossible(vector<vector<int>>& tasks,int mid){
+        for(auto & i : tasks){
+            int actual = i[0];
+            int mini = i[1];
+            if(mini>mid){
+                return false;
+            }
+            mid-=actual;
+        }
+        return true;
+    }
     int minimumEffort(vector<vector<int>>& tasks) {
         int n = tasks.size();
-        sort(tasks.begin(), tasks.end(),
-        [](vector<int>& a, vector<int>& b){
-
-            int diff1 = a[1] - a[0];
-            int diff2 = b[1] - b[0];
-
-            return diff1 > diff2;
+        sort(tasks.begin(),tasks.end(),[](vector<int>&a,vector<int>&b){
+            return (a[1]-a[0])>(b[1]-b[0]);
         });
-        int energy = 0;
-        int used= 0;
-        for(int i=0;i<n;i++){
-            int actual = tasks[i][0];
-            int mini = tasks[i][1];
-            energy = max(energy,used+mini);
-            used+=actual;
+        int l =0;
+        int h= 1e9;
+        int ans = h;
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(isPossible(tasks,mid)){
+                ans = mid;
+                h = mid-1;
+            }
+            else{
+                l=mid+1;
+            }
         }
-        return energy;
+        return ans;
     }
 };
