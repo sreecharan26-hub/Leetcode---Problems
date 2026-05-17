fe130a78a4925b5c22e47319ef19a069ce99bccf
Sree Charan <sreecharanpaila26@gmail.com>
2026-05-17 12:25:19 +0530

    Time: 3 ms (45.57%), Space: 35.2 MB (89.59%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
-        stack<int>st;
-        st.push(start);
-        while(!st.empty()){
-            int i = st.top();
-            st.pop();
+        queue<int>q;
+        q.push(start);
+        while(!q.empty()){
+            int i = q.front();
+            q.pop();
             if(i<0 or i>=n or arr[i]<0){
                 continue;
             }
@@ -15,8 +15,8 @@ public:
             }
             int jmp = arr[i];
             arr[i]*=-1;
-            st.push(i-jmp);
-            st.push(i+jmp);
+            q.push(i-jmp);
+            q.push(i+jmp);
         }
         return false;
     }
