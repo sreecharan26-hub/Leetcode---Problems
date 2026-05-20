80be6ef53e9e7c8289c9fcc32a76aa33ebb47755
Sree Charan <sreecharanpaila26@gmail.com>
2026-05-20 12:22:47 +0530

    Time: 164 ms (40.12%), Space: 163.7 MB (29.01%) - LeetHub

--- 3002-maximum-size-of-a-set-after-removals/3002-maximum-size-of-a-set-after-removals.cpp
+++ 3002-maximum-size-of-a-set-after-removals/3002-maximum-size-of-a-set-after-removals.cpp
@@ -3,14 +3,14 @@ public:
     int maximumSetSize(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(); 
 
-        set<int>s1(nums1.begin(),nums1.end());
-        set<int>s2(nums2.begin(),nums2.end());
+        unordered_set<int>s1(nums1.begin(),nums1.end());
+        unordered_set<int>s2(nums2.begin(),nums2.end());
         int a = s1.size();
         int b = s2.size();
 
         int p = min(a,n/2);
         int q = min(b,n/2);
-        set<int>st;
+        unordered_set<int>st;
         for(auto i : s1){
             st.insert(i);
         }
