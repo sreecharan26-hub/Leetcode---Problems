feb83ccc880482704f34535b7ed1b99b3e087752
Sree Charan <sreecharanpaila26@gmail.com>
2026-06-22 06:10:08 +0530

    Time: 0 ms (100%), Space: 9 MB (64.87%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -5,12 +5,21 @@
             for(auto i : t){
                 v[i-'a']++;
             }
-            return min({
-                v['b'-'a'],
-                v['a'-'a'],
-                v['l'-'a']/2,
-                v['o'-'a']/2,
-                v['n'-'a']
-            });
+            int cnt=0;
+            while(v['b'-'a']>=1 and
+                v['a'-'a']>=1 and 
+                v['l'-'a']>=2 and
+                v['o'-'a']>=2 and
+                v['n'-'a']>=1){
+                
+                v['b'-'a']--; 
+                v['a'-'a']--;
+                v['l'-'a']-=2;
+                v['o'-'a']-=2;
+                v['n'-'a']--;
+
+                cnt++;
+                }
+                return cnt;
         }
     };
