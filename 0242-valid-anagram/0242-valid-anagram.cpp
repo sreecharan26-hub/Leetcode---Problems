1a095e666da99c090d825dfde8c1ba323ffa4b6d
Sree Charan <sreecharanpaila26@gmail.com>
2026-06-22 05:46:12 +0530

    Time: 4 ms (34.67%), Space: 9.6 MB (92.44%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        vector<int>v(26,0);
+        for(auto i : s){
+            v[i-'a']++;
+        }
+        for(auto i : t){
+            v[i-'a']--;
+        }
+        for(auto i : v){
+            if(i>=1){
+                return false;
+            }
+        }
+        return true;
+    }
+};
