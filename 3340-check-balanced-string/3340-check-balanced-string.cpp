0c8bc0d670f7c9837dc444625572de28f4a9802c
Sree Charan <sreecharanpaila26@gmail.com>
2026-06-29 17:47:01 +0530

    Time: 4 ms (2.47%), Space: 8.5 MB (71.64%) - LeetHub

--- 3340-check-balanced-string/3340-check-balanced-string.cpp
+++ 3340-check-balanced-string/3340-check-balanced-string.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     bool isBalanced(string num) {
         int n = num.size();
-        int sume=0;
-        int sumo=0;
-        for(int i=0;i<n;i+=2){
-            sume+=num[i]-'0';
+        int esum = 0;
+        int osum = 0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                esum+=num[i]-'0';
+            }
+            else{
+                osum+=num[i]-'0';
+            }
         }
-        for(int i=1;i<n;i+=2){
-            sumo+=num[i]-'0';
-        }
-        if(sume==sumo){
+        if(esum == osum){
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
 };
