faeccdf3e2e70dca4f858c63d9227749d715b451
Sree Charan <sreecharanpaila26@gmail.com>
2026-07-01 18:00:15 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.85%) - LeetHub

--- 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.cpp
+++ 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     bool consecutiveSetBits(int n) {
-        int prev=0;
-        int cnt =0;
-        while(n>0){
-            int curr = n&1;
-            if(curr==1 and prev ==1){
-                cnt++;
-                if(cnt>1){
-                    return false;
-                }
-            }
-            prev = curr;
-            n>>=1;
+        if(n==0){
+            return false;
         }
-        return cnt ==1;
+        vector<int>v;
+        while(n>0){
+            v.push_back(n%2);
+            n/=2;
+        }
+        int cnt =0;
+        for(int i=0;i<v.size()-1;i++){
+            if(v[i]==1 and v[i+1]==1){
+                cnt++;
+            }
+        }
+        return cnt==1;
     }
 };
