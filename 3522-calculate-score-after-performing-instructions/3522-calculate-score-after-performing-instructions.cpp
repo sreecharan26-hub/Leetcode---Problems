477885983dc79074d141e822b627deb788dbcfb4
Sree Charan <sreecharanpaila26@gmail.com>
2026-07-14 13:13:26 +0530

    Time: 75 ms (9.46%), Space: 183.1 MB (6.76%) - LeetHub

--- 3522-calculate-score-after-performing-instructions/3522-calculate-score-after-performing-instructions.cpp
+++ 3522-calculate-score-after-performing-instructions/3522-calculate-score-after-performing-instructions.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     long long calculateScore(vector<string>& instructions, vector<int>& values) {
-        set<int>st;
+        map<int,int>mp;
         long long sum =0;
         int i=0;
-        while(i>=0 and i<values.size() and st.find(i)==st.end()){
-            st.insert(i);
+        while(i>=0 and i<values.size() and mp.find(i)==mp.end()){
+            mp[i]=1;
             if(instructions[i]=="jump"){
                 i+=values[i];
             }
