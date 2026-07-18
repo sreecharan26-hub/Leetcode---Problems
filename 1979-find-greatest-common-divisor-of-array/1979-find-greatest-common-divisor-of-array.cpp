22ec71765211a25740872ae339294d190561e625
Sree Charan <sreecharanpaila26@gmail.com>
2026-07-18 12:08:53 +0530

    Time: 0 ms (100%), Space: 16.2 MB (31.74%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -4,6 +4,12 @@ public:
         int n = *max_element(nums.begin(),nums.end());
         int m = *min_element(nums.begin(),nums.end());
 
-        return gcd(m,n);
+        int ans = 1;
+        for(int i=1;i<=min(m,n);i++){
+            if(m%i==0 and n%i==0){
+                ans=i;
+            }
+        }
+        return ans;
     }
 };
