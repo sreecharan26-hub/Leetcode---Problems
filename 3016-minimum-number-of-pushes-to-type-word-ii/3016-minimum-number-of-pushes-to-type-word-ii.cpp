52e35cbf1dbb2ba534b96ffff6737662d1f1c321
Sree Charan <sreecharanpaila26@gmail.com>
2026-07-31 16:57:04 +0530

    Time: 5 ms (79.89%), Space: 28.4 MB (31.38%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int sum = 0;
-        unordered_map<char, int> mp;
-        for (auto i : word) {
-            mp[i]++;
-        }
-        vector<int>v;
-        for(auto i: mp){
-            v.push_back(i.second);
+        int n = word.size();
+        vector<int>v(26,0);
+        for(auto i : word){
+            v[i-'a']++;
         }
         sort(v.rbegin(),v.rend());
-        
-        for (int i=0;i<v.size();i++) {
-            if(i < 8) {
-                sum += v[i];
-            }
-            else if(i<16){
-                sum+=2*v[i];
-            }
-            else if(i<24){
-                sum+=3*v[i];
-            }
-            else{
-                sum+=4*v[i];
-            }
-            
+        int sum = 0;
+        for(int i=0;i<26;i++){
+            sum+=v[i]*(i/8+1);
         }
         return sum;
     }
