8dafe469cc3251479e43f8e438cb9c933be986bc
Sree Charan <sreecharanpaila26@gmail.com>
2026-08-03 09:37:59 +0530

    Time: 0 ms (100%), Space: 9.8 MB (62%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+        if(str1+str2 != str2+str1){
+            return "";
+        }
+        int a =gcd(n,m);
+        return str1.substr(0,a);
+    }
+};
