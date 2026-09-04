676946450e6a9ea4d2ddb8aae0c109628cd57a5d
Sree Charan <sreecharanpaila26@gmail.com>
2026-09-04 12:02:21 +0530

    Time: 0 ms (100%), Space: 165.8 MB (84.39%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int n = nums1.size();
-        int ocnt = 0;
-        int ecnt = 0;
-        for(auto i : nums1){
-            if(i%2==0){
-                ecnt++;
-            }
-            else{
-                ocnt++;
-            }
-        }
-        if(ocnt==0 or ecnt==0){
-            return true;
-        }
+        // int n = nums1.size();
+        // int ocnt = 0;
+        // int ecnt = 0;
+        // for(auto i : nums1){
+        //     if(i%2==0){
+        //         ecnt++;
+        //     }
+        //     else{
+        //         ocnt++;
+        //     }
+        // }
+        // if(ocnt==0 or ecnt==0){
+        //     return true;
+        // }
         int a  = *min_element(nums1.begin(),nums1.end());
         if(a%2!=0){
             return true;
